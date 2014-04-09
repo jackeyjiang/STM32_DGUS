97aa613dcbd409cb68253d380867ea6e80f60df4
江冲 <jackeyjiang123@gmail.com>
2014-04-09 23:43:17 +0800

    deleted .o .i .crf

--- STM32_Demo/projects/test/mdk/debug/ccsbcs.d
+++
@@ -1,4 +0,0 @@
-.\debug\ccsbcs.o: ..\..\..\filesys\ff\src\option\ccsbcs.c
-.\debug\ccsbcs.o: ..\..\..\filesys\ff\src\option\../ff.h
-.\debug\ccsbcs.o: ..\..\..\filesys\ff\src\option\../integer.h
-.\debug\ccsbcs.o: ..\..\..\filesys\ff\src\option\../ffconf.h
--- STM32_Demo/projects/test/mdk/debug/ff.d
+++
@@ -1,6 +0,0 @@
-.\debug\ff.o: ..\..\..\filesys\ff\src\ff.c
-.\debug\ff.o: ..\..\..\filesys\ff\src\ff.h
-.\debug\ff.o: ..\..\..\filesys\ff\src\integer.h
-.\debug\ff.o: ..\..\..\filesys\ff\src\ffconf.h
-.\debug\ff.o: ..\..\..\filesys\ff\src\diskio.h
-.\debug\ff.o: C:\Keil\ARM\ARMCC\bin\..\include\stdarg.h
